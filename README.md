8e3d71a4000a2379dca700185887d984c7143512
Rolf <r10@eebe.be>
2017-11-20 20:15:10 +0200

    Fetch SHA1 for all files

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # backblaze-b2-dedupe
 Deduplicate files before uploading to Backblaze B2
+
+This is work in progress. At the moment, it only fetches the checksums, the next step is to compare with local files and de-duplicate.
+
+The "local" directory is not committed, because it contains temporary and sensitive information (backblaze api key, for example).
+
+It should exist and contain a file named config.js, with the information resembling the following:
+
+```
+{
+	"bucketId": "cdf6711c606e3a711367f427",
+    "accountId": "364fd60cfdad",
+    "applicationKey": "000c60fa6031e86d64d6e7faa070f711cf2acb390a"
+}
+```
+
+When run (`node index.js`), this script will connect to the specified bucket and try to fetch the SHA1 checksum for **all files**. 
+
+Charges can be incurred - see the B2 site for more info.
+
+The SHA1 checksums will be saved in local/remoteHashes.txt, with one line per file, containing the checksum, then a space, then the path of the file relative to the bucket root, then a newline (\n).
+
+```
+6bc5aa1d824b35e4cb09429eee74feccbfbe8c42 myPhotos/Updloads/3340175.ORF
+```
--- index.js
+++ index.js
@@ -1,14 +1,65 @@
-const sha1File = require("sha1-file");
+// const sha1File = require("sha1-file");
 const B2 = require("backblaze-b2");
 const fs = require("fs");
 
+const localDir = 'local/';
+
 // load credentials
-var b2Credentials = JSON.parse(fs.readFileSync('local/credentials.js', 'utf8', function (err, data) {
+const b2Config = JSON.parse(fs.readFileSync(localDir + 'config.js', 'utf8', function (err, data) {
     if (err) throw err; // we'll not consider error handling for now
 }));
 
+const remoteHashListFileName = localDir + 'remoteHashes.txt';
+fs.truncate(remoteHashListFileName, 0, function(){
+	// file is now empty
+});
+
 // create b2 object instance
-var b2 = new B2(b2Credentials);
+var b2 = new B2({
+    "accountId": b2Config.accountId,
+    "applicationKey": b2Config.applicationKey
+});
+
+// console.log(sha1File("local/P9140797.ORF"));
+
+// takes a B2 listFileNames response and appends it to a file
+function saveListFileNamesResponse(r) {
+	var lines = "";
+	r.data.files.forEach((f) => {
+		lines += f.contentSha1 + " " + f.fileName + "\n";
+	});
+	fs.appendFile(remoteHashListFileName, lines, function(err){
+		// my code is perfect
+	});
+}
+
+async function getAllFileNames(saveResponse) {
+	var r;
+	var nextFileName = "";
+
+	while(nextFileName !== null) {
+		try {
+			r = await b2.listFileNames({
+			    bucketId: b2Config.bucketId,
+			    startFileName: nextFileName,
+			    maxFileCount: 5000,
+			    delimiter: '',
+			    prefix: ''
+			});
+			nextFileName = r.data.nextFileName;
+			if(typeof saveResponse == "function") {
+				saveResponse(r);
+			}
+		} catch (e) {
+			console.dir(e);
+		}
+	}
+}
+
+b2.authorize().then(() => {
+	getAllFileNames(saveListFileNamesResponse);
+});
+
 
 /*
 async function GetBuckets() {
@@ -22,15 +73,14 @@ async function GetBuckets() {
 }
 */
 
-console.log(sha1File("local/P9140797.ORF"));
-
+/*
 
 b2.authorize().then(
 	() => {
 		return b2.listFileNames({
 		    bucketId: 'b4d47c917831c1d95dfd021a',
-		    startFileName: "P9140797.ORF",
-		    maxFileCount: 1,
+		    startFileName: "",
+		    maxFileCount: 0,
 		    delimiter: '',
 		    prefix: ''
 		});
@@ -41,14 +91,17 @@ b2.authorize().then(
 ).then(
 	(response) => {
 		response.data.files.forEach(function(f){
-			console.log([f.fileName, f.contentSha1]);
+			console.log("[" + f.fileName + ", " + f.contentSha1 + "]");
 		})
 	},
 	(err) => {
 		console.log("List fail");
 		console.dir(err.response.data);
+		console.log(err.response.headers['content-length']);
 	}
 );
+
+*/
 
 
 
